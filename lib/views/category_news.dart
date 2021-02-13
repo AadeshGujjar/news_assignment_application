c398570386e3f2e3702b1df63910c7fb5f8c4bb6
Aadesh Gujjar <aadesh.gujjar25@gmail.com>
2021-02-13 18:24:44 +0530

    Initial commit

---
+++ lib/views/category_news.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CotegoryNews extends StatefulWidget {
+  @override
+  _CotegoryNewsState createState() => _CotegoryNewsState();
+}
+
+class _CotegoryNewsState extends State<CotegoryNews> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
