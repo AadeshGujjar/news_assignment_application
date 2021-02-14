13df7c692eab01b0f555b28e323e672bd8b1c5b4
Aadesh Gujjar <aadesh.gujjar25@gmail.com>
2021-02-14 19:47:06 +0530

    Main Page UI complete.

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 class News{
   List<ArticleModel> news = [];
   Future<void> getNews() async{
-    String url= "https://newsapi.org/v2/top-headlines?country=us&apiKey=7d730577062c469094d352562061ffa5";
+    String url= "https://newsapi.org/v2/top-headlines?country=in&apiKey=7d730577062c469094d352562061ffa5";
 
     var response= await http.get(url);
     var jsonData= jsonDecode(response.body);
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -53,41 +54,47 @@ class _HomeState extends State<Home> {
         ),
       ):
 
-      Container(
-        child: Column(
-          children: <Widget>[
-            ///categories
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              height: 70,
-              child: ListView.builder(
-                itemCount: categories.length,
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context,index){
-                  return CategoryTile(
-                    imageUrl: categories[index].imageUrl,
-                    categoryName: categories[index].categoryName,
-                  );
-                },
-              ),
-            ),
-            ///blogs
-            Container(
-              child: ListView.builder(
-                itemCount: articles.length,
-                  shrinkWrap: true,
-                  itemBuilder: (context,index){
-                  return BlogTile(
-                  imageUrl: articles[index].urlToImage,
-                  title:articles[index].title,
-                  description: articles[index].description,
-                  );
-              },
-              ),
-            )
+      SingleChildScrollView(
 
-          ],
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column( ///column replacement
+            children: <Widget>[
+              ///categories
+              Container(
+
+                height: 70,
+                child: ListView.builder(
+                  itemCount: categories.length,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context,index){
+                    return CategoryTile(
+                      imageUrl: categories[index].imageUrl,
+                      categoryName: categories[index].categoryName,
+                    );
+                  },
+                ),
+              ),
+              ///blogs
+              Container(
+                padding: EdgeInsets.only(top: 16),
+                child: ListView.builder(
+                  itemCount: articles.length,
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    itemBuilder: (context,index){
+                    return BlogTile(
+                    imageUrl: articles[index].urlToImage,
+                    title:articles[index].title,
+                    description: articles[index].description,
+                    );
+                },
+                ),
+              )
+
+            ],
+          ),
         ),
       ),
 
@@ -112,7 +119,8 @@ class CategoryTile extends StatelessWidget {
         children: <Widget>[
           ClipRRect(
             borderRadius: BorderRadius.circular(6.0),
-              child: Image.network(imageUrl, width: 120, height: 60, fit: BoxFit.cover)),
+              child: CachedNetworkImage(
+                  imageUrl: imageUrl, width: 120, height: 60, fit: BoxFit.cover)),
 
           Container(
             alignment: Alignment.center,
@@ -141,12 +149,22 @@ class BlogTile extends StatelessWidget {
 
     @override
   Widget build(BuildContext context) {
-    return Container( 
+    return Container(
+      margin: EdgeInsets.only(bottom: 16),
       child: Column(
         children: <Widget>[
-          Image.network(imageUrl),
-        Text(title),
-        Text(description),
+          ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: Image.network(imageUrl)),
+        Text(title, style: TextStyle(
+          fontSize: 17,
+          color: Colors.black87,
+          fontWeight: FontWeight.w500
+        ) ,),
+        SizedBox(height: 8.0,),
+        Text(description, style:  TextStyle(
+            color: Colors.black54
+        ),),
         ],
       ),
     );
