7db09c6488f7aa6155d3ece9d085d2657cbab72b
Aadesh Gujjar <aadesh.gujjar25@gmail.com>
2021-02-14 20:18:02 +0530

    Second page navigation start.

--- lib/views/article_views.dart
+++ lib/views/article_views.dart
@@ -1,13 +1,57 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticleView extends StatefulWidget {
+  final String blogUrl;
+
+  ArticleView({this.blogUrl}) ;
+
   @override
   _ArticleViewState createState() => _ArticleViewState();
 }
 
 class _ArticleViewState extends State<ArticleView> {
+
+  final Completer<WebViewController> _completer=Completer<WebViewController>();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("News"),
+            Text(" Assignment ", style: TextStyle(
+                color: Colors.blue
+            ),),
+            Text("App"),
+          ],
+        ),
+        actions: <Widget>[
+          Opacity(
+            opacity: 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+
+            ),
+          )
+        ],
+
+        elevation: 0.0,
+      ),
+      body: Container(
+        
+        child: WebView(
+
+          initialUrl: widget.blogUrl,
+          onWebViewCreated: (( WebViewController webViewController){
+            _completer.complete(webViewController);
+
+          }),
+        ),
+      ),
+    );
   }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -6,6 +6,7 @@ import 'package:news_assignment_application/helper/data.dart';
 import 'package:news_assignment_application/helper/news.dart';
 import 'package:news_assignment_application/models/article_model.dart';
 import 'package:news_assignment_application/models/category_model.dart';
+import 'package:news_assignment_application/views/article_views.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -88,6 +89,7 @@ class _HomeState extends State<Home> {
                     imageUrl: articles[index].urlToImage,
                     title:articles[index].title,
                     description: articles[index].description,
+                     url: articles[index].url,
                     );
                 },
                 ),
@@ -144,28 +146,38 @@ class CategoryTile extends StatelessWidget {
 
 class BlogTile extends StatelessWidget {
 
-  final String imageUrl,title, description;
-  BlogTile({@required this.imageUrl,@required this.title,@required this.description});
+  final String imageUrl,title, description, url;
+  BlogTile({@required this.imageUrl,@required this.title,@required this.description, @required this.url});
 
     @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 16),
-      child: Column(
-        children: <Widget>[
-          ClipRRect(
-              borderRadius: BorderRadius.circular(6),
-              child: Image.network(imageUrl)),
-        Text(title, style: TextStyle(
-          fontSize: 17,
-          color: Colors.black87,
-          fontWeight: FontWeight.w500
-        ) ,),
-        SizedBox(height: 8.0,),
-        Text(description, style:  TextStyle(
-            color: Colors.black54
-        ),),
-        ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+            builder: (context) => ArticleView(
+
+
+            )
+        ),);
+      },
+       child: Container(
+        margin: EdgeInsets.only(bottom: 16),
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Image.network(imageUrl)),
+          Text(title, style: TextStyle(
+            fontSize: 17,
+            color: Colors.black87,
+            fontWeight: FontWeight.w500
+          ) ,),
+          SizedBox(height: 8.0,),
+          Text(description, style:  TextStyle(
+              color: Colors.black54
+          ),),
+          ],
+        ),
       ),
     );
   }
